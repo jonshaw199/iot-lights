b819abd2f6657c4a29aa000e615ad99aa3d25405
Jon <jonshaw199@gmail.com>
2022-08-07 11:23:34 -0700

    Logging

--- src/stateent/handshake/slaveHandshake.cpp
+++ src/stateent/handshake/slaveHandshake.cpp
@@ -5,11 +5,17 @@ void SlaveHandshake::loop()
 {
   if (MessageHandler::getInbox().size())
   {
+    Serial.println("Messages in inbox");
     JSMessage m = MessageHandler::getAndPop();
     if (m.getType() == TYPE_HANDSHAKE_REQUEST)
     {
+      Serial.println("Handshake message in inbox");
       MessageHandler::receiveHandshakeRequest(m);
       MessageHandler::sendHandshakeResponses({m.getSenderID()});
+    }
+    else
+    {
+      Serial.println("Not a message we care about");
     }
   }
 }
