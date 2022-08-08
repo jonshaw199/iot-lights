c15555c6ab02f6e6af55a3bed7f20571ff621218
Jon <jonshaw199@gmail.com>
2022-08-08 15:28:46 -0700

    Using outbox for as much as possible instead of sending directly

--- src/stateent/espent/master/master.cpp
+++ src/stateent/espent/master/master.cpp
@@ -7,15 +7,16 @@
 
 void Master::loop()
 {
-  Base::loop();
-
   JSMessage msg;
   msg.setState(STATE_RUN);
   msg.setType(TYPE_RUN_DATA);
   msg.setColor(LED::getRandColor());
   msg.setMaxRetries(DEFAULT_RETRIES);
 
-  MessageHandler::sendMsg(msg);
+  // MessageHandler::sendMsg(msg);
+  MessageHandler::getOutbox().push(msg);
+
+  Base::loop();
 
   delay(DELAY_MASTER_LOOP);
 }
