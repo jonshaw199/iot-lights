75e93be195d59e3fca76fa894be09aad1ff6da82
Jon <jonshaw199@gmail.com>
2022-08-21 18:50:47 -0700

    Stripping code from duplicated demo2; adding demo3 state

--- src/state.h
+++ src/state.h
@@ -4,7 +4,8 @@
 enum js_state_extended
 {
   STATE_DEMO1 = 10,
-  STATE_DEMO2 = 11
+  STATE_DEMO2 = 11,
+  STATE_DEMO3 = 12
 };
 
 #endif // STATE_H_
--- src/stateEnt/demo3/master/demo3Master.h
+++ src/stateEnt/demo3/master/demo3Master.h
@@ -6,15 +6,9 @@
 class demo3Master : public Base
 {
   static bool demo3(IECBArg a);
-  static void sendMsg(uint8_t b);
-  static const float coefs[];
-  static const unsigned long sceneMs;
-  static float getCurCoef(unsigned long elapsedMs);
-  static const uint8_t maxBrightness;
 
 public:
   demo3Master();
-  void setup();
 };
 
 #endif // STATEENT_DEMO3_MASTER_DEMO3MASTER_H_
