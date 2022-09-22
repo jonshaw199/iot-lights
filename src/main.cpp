7a7c8b9fcf29e491293563d04d35b34f0738ce06
Jon Shaw <jonshaw199@gmail.com>
2022-09-21 19:45:12 -0700

    Increasing setTimeout for serial input; adding test states back

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,31 @@
 #include "state.h"
 #include "stateEnt/home/home.h"
 #include "stateEnt/show/show.h"
+#include "stateEnt/test/blink/blink.h"
+#include "stateEnt/test/sync/sync.h"
 
 void setup()
 {
   Serial.begin(JS_BAUD);
+  Serial.setTimeout(JS_SERIAL_TIMEOUT);
   AF1::setup(JS_ID);
 #ifdef JS_IP_A
-  AF1::registerWifiAP("js-guest", "B1g5lams!lol", JS_IP_A, JS_IP_B, JS_IP_C, JS_IP_D, 192, 168, 1, 254, 255, 255, 255, 0);
+  AF1::registerWifiAP("js-guest", "B1g5lams!", JS_IP_A, JS_IP_B, JS_IP_C, JS_IP_D, 192, 168, 1, 254, 255, 255, 255, 0);
 #else
-  AF1::registerWifiAP("js-guest", "B1g5lams!lol");
+  AF1::registerWifiAP("js-guest", "B1g5lams!");
 #endif
   AF1::registerStateEnt(STATE_HOME, new Home());
   AF1::registerStateEnt(STATE_SHOW, new Show());
+  AF1::registerStateEnt(STATE_BLINK, new Blink());
+  AF1::registerStateEnt(STATE_SYNC, new Sync());
   AF1::registerStringHandler("1", []()
                              { AF1::setRequestedState(STATE_HOME); });
   AF1::registerStringHandler("2", []()
                              { AF1::setRequestedState(STATE_SHOW); });
+  AF1::registerStringHandler("3", []()
+                             { AF1::setRequestedState(STATE_BLINK); });
+  AF1::registerStringHandler("4", []()
+                             { AF1::setRequestedState(STATE_SYNC); });
 }
 
 void loop()
