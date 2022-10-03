e47a41fe1f9d8d09a4e47fd917b1f19c9c99314d
Jon Shaw <jonshaw199@gmail.com>
2022-10-03 06:50:44 -0700

    m5stickc; apparently need to call m5.begin() before io pins can be used

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 
 #include <AF1.h>
 
+#ifdef ARDUINO_M5Stick_C
+#include <M5StickCPlus.h> // https://github.com/m5stack/M5Stack/issues/97
+#undef min
+#endif
+
 #include "state.h"
 #include "stateEnt/home/home.h"
 #include "stateEnt/song1/song1.h"
 
 void setup()
 {
-  Serial.begin(JS_BAUD);
   Serial.setTimeout(JS_SERIAL_TIMEOUT);
+  Serial.begin(JS_BAUD);
+#ifdef ARDUINO_M5Stick_C
+  M5.begin();
+#endif
   AF1::setup(JS_ID);
 #ifdef JS_IP_A
   AF1::registerWifiAP(JSSSID, JSPASS, JS_IP_A, JS_IP_B, JS_IP_C, JS_IP_D, 192, 168, 1, 254, 255, 255, 255, 0);
