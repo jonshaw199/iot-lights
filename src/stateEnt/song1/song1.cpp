99b847cb91c899bc43106829471797aafd45ad57
Jon Shaw <jonshaw199@gmail.com>
2022-10-09 07:54:16 -0700

    Checking if LED count is greater than 0 before doing stuff and things

--- src/stateEnt/song1/song1.cpp
+++ src/stateEnt/song1/song1.cpp
@@ -6,9 +6,15 @@ CRGB Song1::ledsB[CNT_B];
 
 Song1::Song1()
 {
-  FastLED.addLeds<LED_TYPE_A, LED_PIN_A, LED_ORDER_A>(ledsA, CNT_A);
-  FastLED.addLeds<LED_TYPE_B, LED_PIN_B, LED_ORDER_B>(ledsB, CNT_B);
-  FastLED.setBrightness(50);
+  if (CNT_A)
+  {
+    FastLED.addLeds<LED_TYPE_A, LED_PIN_A, LED_ORDER_A>(ledsA, CNT_A);
+  }
+  if (CNT_B)
+  {
+    FastLED.addLeds<LED_TYPE_B, LED_PIN_B, LED_ORDER_B>(ledsB, CNT_B);
+  }
+  FastLED.setBrightness(150);
   // FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
 }
 
@@ -16,8 +22,15 @@ void Song1::preStateChange(int s)
 {
   Base::preStateChange(s);
   // Turn off lights
-  fill_solid(ledsA, CNT_A, CRGB::Black);
-  fill_solid(ledsB, CNT_B, CRGB::Black);
+  if (CNT_A)
+  {
+    fill_solid(ledsA, CNT_A, CRGB::Black);
+  }
+  if (CNT_B)
+  {
+    fill_solid(ledsB, CNT_B, CRGB::Black);
+  }
+
   FastLED.show();
 }
 
@@ -34,8 +47,12 @@ void Song1::doSynced()
       {
   uint8_t beatA = beatsin8(17, 0, 255);                        // Starting hue
   uint8_t beatB = beatsin8(13, 0, 255);
-  fill_rainbow(ledsA, CNT_A, (beatA+beatB)/2, 8); 
-  fill_rainbow(ledsB, CNT_B, (beatA+beatB)/2, 8); 
+  if (CNT_A) {
+fill_rainbow(ledsA, CNT_A, (beatA+beatB)/2, 8); 
+  }
+  if (CNT_B) {
+    fill_rainbow(ledsB, CNT_B, (beatA+beatB)/2, 8); 
+  }
   FastLED.show();
   return true; },
       -1, true);
