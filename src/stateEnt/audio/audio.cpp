10a7b2ee433127811fb4b07084f2578f3f24288f
Jon Shaw <jonshaw199@gmail.com>
2022-10-15 16:15:19 -0700

    KISS

--- src/stateEnt/audio/audio.cpp
+++ src/stateEnt/audio/audio.cpp
@@ -121,11 +121,11 @@ public:
 
     // Play one file, don't return until complete
     Serial.println(F("Playing track 001"));
-    // musicPlayer.playFullFile("/track001.mp3");
+    musicPlayer.playFullFile("/track001.mp3");
     // Play another file in the background, REQUIRES interrupts!
     // Serial.println(F("Playing track 002"));
     // musicPlayer.startPlayingFile("/track002.mp3");
-    musicPlayer.startPlayingFile("/track001.mp3");
+    // musicPlayer.startPlayingFile("/track001.mp3");
   }
 
   void pausePlaying(bool p)
