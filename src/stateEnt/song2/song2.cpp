c15071b2c8d644b9c9e55830a957e08c6da68357
Jon Shaw <jonshaw199@gmail.com>
2022-10-26 20:28:02 -0700

    defaults (doesn't work with lots of LEDs so not using anymore)

--- src/stateEnt/song2/song2.cpp
+++ src/stateEnt/song2/song2.cpp
@@ -33,8 +33,8 @@ void Song2::setup()
     }
     FastLED.setBrightness(200);
     // FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
-    // setupStripes();
-    setupFire();
+    setupStripes();
+    // setupFire();
     // setupNoise();
     // setupBreathing();
     // setupDisco();
@@ -215,12 +215,12 @@ void Song2::setupStripes()
 // COOLING: How much does the air cool as it rises?
 // Less cooling = taller flames.  More cooling = shorter flames.
 // Default 50, suggested range 20-100
-#define COOLING 25
+#define COOLING 50
 
 // SPARKING: What chance (out of 255) is there that a new spark will be lit?
 // Higher chance = more roaring fire.  Lower chance = more flickery fire.
 // Default 120, suggested range 50-200.
-#define SPARKING 190
+#define SPARKING 120
 
 void Song2::setupFire()
 {
