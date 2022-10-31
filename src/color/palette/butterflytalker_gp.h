22110a7d38bca15d7d68299e1956550d28d809ec
Jon Shaw <jonshaw199@gmail.com>
2022-10-30 20:43:54 -0700

    butterflytalker palette

---
+++ src/color/palette/butterflytalker_gp.h
@@ -0,0 +1,21 @@
+
+#ifndef COLOR_PALETTE_BUTTERFLYTALKERGP_H_
+#define COLOR_PALETTE_BUTTERFLYTALKERGP_H_
+
+#include <FastLED.h>
+
+// Gradient palette "butterflytalker_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/butterflytalker.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(butterflytalker_gp){
+    0, 1, 1, 6,
+    51, 6, 11, 52,
+    89, 107, 107, 192,
+    127, 101, 161, 192,
+    165, 107, 107, 192,
+    204, 6, 11, 52,
+    255, 0, 0, 0};
+
+#endif
--- src/stateEnt/pattern/twinklefox/twinklefox.cpp
+++ src/stateEnt/pattern/twinklefox/twinklefox.cpp
@@ -1,12 +1,10 @@
 #include <FastLED.h>
 
 #include "twinklefox.h"
-#include "color/palette/autumnrose_gp.h"
-#include "color/palette/blackhorse_gp.h"
-#include "color/palette/flame_gp.h"
 #include "color/palette/halloween_gp.h"
 #include "color/palette/hopegoddess_gp.h"
 #include "color/palette/leo_gp.h"
+#include "color/palette/butterflytalker_gp.h"
 
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
@@ -392,7 +390,7 @@ const TProgmemRGBPalette16 *ActivePaletteList[] = {
 // Advance to the next color palette in the list (above).
 void Twinklefox::chooseNextColorPalette(CRGBPalette16 &pal)
 {
-  const CRGBPalette16 active[] = {halloween_gp, hopegoddess_gp, leo_gp};
+  const CRGBPalette16 active[] = {halloween_gp, hopegoddess_gp, leo_gp, butterflytalker_gp};
   const uint8_t numberOfPalettes = sizeof(active) / sizeof(active[0]);
   static uint8_t whichPalette = -1;
   whichPalette = addmod8(whichPalette, 1, numberOfPalettes);
